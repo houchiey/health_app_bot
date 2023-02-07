e8549bea55d37733110c412d7fb67348baa8a68f
Hayato Ouchi <tbh.kyomei0705@gmail.com>
2023-02-07 13:47:32 +0000

    Initial commit

---
+++ test/health_app_bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HealthAppBot from '../lib/health_app_bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/health_app_bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HealthAppBot.HealthAppBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
